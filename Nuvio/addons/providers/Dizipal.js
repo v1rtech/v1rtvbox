71ba2902dd726bb70745b1789ccb7d52a00b79f4
v1rtech <burak.kahraman@ayakkabionline.com>
2026-09-18 18:00:58 +0300

    new providers fix1

--- Nuvio/addons/providers/Dizipal.js
+++ Nuvio/addons/providers/Dizipal.js
@@ -1,5 +1,5 @@
 /**
- * patronDizipal - Built from src/patronDizipal/
+ * V1RDizipal - Built from src/V1RDizipal/
  * Generated: 2026-08-23T16:48:56.584Z
  */
 var __create = Object.create;
@@ -75,7 +75,7 @@ __export(patronDizipal_exports, {
 module.exports = __toCommonJS(patronDizipal_exports);
 
 // src/patronDizipal/http.js
-var MAIN_URL = "https://dizipal2115.com";
+var MAIN_URL = "https://dizipal1582.com";
 var HEADERS = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
